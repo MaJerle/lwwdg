2a45e7a2d6f5c59bf96914d5ecc5a59d1c05cab5
Tilen Majerle <tilen@majerle.eu>
2024-03-19 21:34:26 +0100

    Update library CMake system, to support automatic creation of options file, if user doesn't provide one

--- lwwdg/library.cmake
+++ lwwdg/library.cmake
@@ -1,13 +1,18 @@
 # 
+# LIB_PREFIX: LWWDG
+#
 # This file provides set of variables for end user
 # and also generates one (or more) libraries, that can be added to the project using target_link_libraries(...)
 #
 # Before this file is included to the root CMakeLists file (using include() function), user can set some variables:
 #
-# LWWDG_OPTS_DIR: If defined, it should set the folder path where options file shall be generated.
+# LWWDG_OPTS_FILE: If defined, it is the path to the user options file. If not defined, one will be generated for you automatically
 # LWWDG_COMPILE_OPTIONS: If defined, it provide compiler options for generated library.
 # LWWDG_COMPILE_DEFINITIONS: If defined, it provides "-D" definitions to the library build
 #
+
+# Custom include directory
+set(LWWDG_CUSTOM_INC_DIR ${CMAKE_CURRENT_BINARY_DIR}/lib_inc)
 
 # Library core sources
 set(lwwdg_core_SRCS 
@@ -17,6 +22,7 @@ set(lwwdg_core_SRCS
 # Setup include directories
 set(lwwdg_include_DIRS
     ${CMAKE_CURRENT_LIST_DIR}/src/include
+    ${LWWDG_CUSTOM_INC_DIR}
 )
 
 # Register library to the system
@@ -26,7 +32,11 @@ target_include_directories(lwwdg INTERFACE ${lwwdg_include_DIRS})
 target_compile_options(lwwdg PRIVATE ${LWWDG_COMPILE_OPTIONS})
 target_compile_definitions(lwwdg PRIVATE ${LWWDG_COMPILE_DEFINITIONS})
 
-# Create config file
-if(DEFINED LWWDG_OPTS_DIR AND NOT EXISTS ${LWWDG_OPTS_DIR}/lwwdg_opts.h)
-    configure_file(${CMAKE_CURRENT_LIST_DIR}/src/include/lwwdg/lwwdg_opts_template.h ${LWWDG_OPTS_DIR}/lwwdg_opts.h COPYONLY)
+# Create config file if user didn't provide one info himself
+if(NOT LWWDG_OPTS_FILE)
+    message(STATUS "Using default lwwdg_opts.h file")
+    set(LWWDG_OPTS_FILE ${CMAKE_CURRENT_LIST_DIR}/src/include/lwwdg/lwwdg_opts_template.h)
+else()
+    message(STATUS "Using custom lwwdg_opts.h file from ${LWWDG_OPTS_FILE}")
 endif()
+configure_file(${LWWDG_OPTS_FILE} ${LWWDG_CUSTOM_INC_DIR}/lwwdg_opts.h COPYONLY)
